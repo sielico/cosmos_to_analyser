119a6a2b6e92947f20e214c3046fe4e9777a9345
Egawa Yoshiki <egawa.yoshiki.0323@gmail.com>
2016-09-24 23:57:49 +0900

    古い譜面以外は対応

--- a.js
+++ a.js
@@ -1,6 +1,31 @@
 (function(){
-  var base_html = document.getElementById("wikibody").innerHTML;
+  var base_html;
+  if(document.getElementById("wikibody")!=null){
+    base_html = document.getElementById("wikibody").innerHTML;
+  }else{
+    var temp = document.body.innerHTML;
+    base_html = temp.substring(temp.indexOf("@@@@"));
+  }
   var m = base_html.match(/[^p]*.*?<p>[\S\s]*?(.*?)<br.*?[\S\s](.*?)<br.*?[\S\s]<br.*?[\S\s](BASIC|ADVANCED|EXTREME)<br.*?[\S\s]<br.*?[\S\s]Le.*?(\d+).*?[\S\s]BP.*?(\d+).*?[\S\s]No.*?\d+.*?<br.*?[\S\s].*?<br.*?[\S\s]/m);
-  var fumen = base_html.substring(m[0].length+1, base_html.indexOf("不"));
-  console.log(fumen);
+  console.log(m);
+  var m0 = m[0];
+  var m1 = m[1];
+  var m2 = m[2];
+  var m3 = m[3];
+  var m4 = m[4];
+  var m5 = m[5];
+  var t = "t=" + m5 + "<br />";
+  var o = "o=0<br />";
+  var m = '//m="' + m1 + '.mp3"<br />';
+  var level = "#lev=" + m4 + "<br />";
+  var title = '#artist="' + m2 + '"<br />';
+  var jacket = "//#jacket=\"" + m1 + '.png"<br />';
+  var memo2 = "#memo2<br />";
+  var fumen = base_html.substring(m0.length+1, base_html.indexOf("不"));
+  var hold = "";
+  if(fumen.indexOf("href")>0){
+    hold = "#holdbarrow=1<br />";
+  }
+  var out_html = "<div align=left>" + t + o + m + level + title + jacket + hold + memo2 + fumen + "</div>";
+  document.body.innerHTML = out_html;
 })()
